20f101bbab35237a54e3c9aad88edacd64f0af8b
Sergey Vyatkin <vyatkinhome@yahoo.com>
2020-09-21 16:43:03 -0700

    New tests

--- src/test/java/com/javacodegeeks/examples/junitmavenexample/CalculatorTest.java
+++ src/test/java/com/javacodegeeks/examples/junitmavenexample/CalculatorTest.java
@@ -26,45 +26,69 @@ public class CalculatorTest {
 	public void afterEachTest() {
 		System.out.println("This is executed after each Test");
 	}
+//
+//	@Test
+//	public void testSum() {
+//		int result = calculator.sum(3, 4);
+//		assertEquals(7, result);
+//
+//		int result1 = calculator.sum(4, 4);
+//		assertEquals(8, result1);
+//	}
+//
+//	@Test
+//	public void testDivision() {
+//		try {
+//			int result = calculator.division(10, 2);
+//			assertEquals(5, result);
+//		} catch (Exception e) {
+//			e.printStackTrace(System.err);
+//		}
+//	}
+//
+//	@Test(expected = Exception.class)
+//	public void testDivisionException() throws Exception {
+//		calculator.division(10, 0);
+//	}
+//
+//	@Test
+//	public void testEqual() {
+//		boolean result = calculator.equalIntegers(20, 20);
+//		assertTrue(result);
+//	}
+//
+//	@Test
+//	public void testSubtraction() {
+//		int result = calculator.subtraction(10,1);
+//		assertEquals(9, result);
+//	}
 
 	@Test
-	public void testSum() {
+	public void testTestSum()
+	{
 		int result = calculator.sum(3, 4);
-
 		assertEquals(7, result);
-
-		int result1 = calculator.sum(4, 4);
-
-		assertEquals(8, result1);
 	}
 
 	@Test
-	public void testDivision() {
-		try {
-			int result = calculator.division(10, 2);
-
-			assertEquals(5, result);
-		} catch (Exception e) {
-			e.printStackTrace(System.err);
-		}
-	}
-
-	@Test(expected = Exception.class)
-	public void testDivisionException() throws Exception {
-		calculator.division(10, 0);
-	}
-
-	@Test
-	public void testEqual() {
-		boolean result = calculator.equalIntegers(20, 20);
-
-		assertTrue(result);
-	}
-
-	@Test
-	public void testSubtraction() {
+	public void testTestSubtraction()
+	{
 		int result = calculator.subtraction(10,1);
-
 		assertEquals(9, result);
+	}
+
+	@Test
+	public void testMultiplication()
+	{
+		int result = calculator.multiplication(10,1);
+		assertEquals(10, result);
+	}
+
+	public void testTestDivision()
+	{
+	}
+
+	public void testEqualIntegers()
+	{
 	}
 }
