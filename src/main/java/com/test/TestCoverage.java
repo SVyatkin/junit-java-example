6a934cca41126417e9593cb7bdfbeec7720f6788
Sergey Vyatkin <vyatkinhome@yahoo.com>
2020-09-21 19:35:13 -0700

    Clean code

--- src/main/java/com/test/TestCoverage.java
+++ src/main/java/com/test/TestCoverage.java
@@ -4,7 +4,6 @@ public class TestCoverage
 {
     public String testMethod(String str){
         StringBuffer sb = new StringBuffer(str);
-        String s = sb.reverse().toString();
-        return s;
+        return sb.reverse().toString();
     }
 }
